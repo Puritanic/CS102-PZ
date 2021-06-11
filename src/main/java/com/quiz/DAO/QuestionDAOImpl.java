513d75aea7a34cfb221e23aefddfe0228a22752d
Darkø Tasevski <darko.tasevski@redbox.com>
2021-06-11 19:51:28 +0200

    refactor: initial GameView data fetching impl

--- src/main/java/com/quiz/DAO/QuestionDAOImpl.java
+++ src/main/java/com/quiz/DAO/QuestionDAOImpl.java
@@ -1,4 +1,22 @@
 package com.quiz.DAO;
 
+import com.quiz.entities.Question;
+import com.quiz.util.HibernateUtil;
+import org.hibernate.Session;
+
+import java.util.List;
+
 public class QuestionDAOImpl {
+    public QuestionDAOImpl(){}
+
+    public List<Question> getQuestions(){
+        Session session = HibernateUtil.getCurrentSession();
+        session.beginTransaction();
+
+        List<Question> questions = session.createQuery("from Question").getResultList();
+
+        session.getTransaction().commit();
+
+        return questions;
+    }
 }
--- src/main/java/com/quiz/app/views/GameView.java
+++ src/main/java/com/quiz/app/views/GameView.java
@@ -1,10 +1,24 @@
 package com.quiz.app.views;
 
+import com.quiz.DAO.QuestionDAOImpl;
+import com.quiz.entities.Question;
 import com.quiz.interfaces.BaseView;
 import javafx.scene.layout.BorderPane;
+
+import java.util.List;
 
 public class GameView extends BorderPane implements BaseView {
     public GameView(){
         System.out.println("GameView loaded");
+        QuestionDAOImpl quest = new QuestionDAOImpl();
+        List<Question> questions = quest.getQuestions();
+
+        System.out.println(questions);
+
+        for (Question q:questions) {
+            System.out.println();
+            System.out.println(q);
+            System.out.println();
+        }
     }
 }
